349e10b09c278b9f709b7e137f49bad4990c8857
Iyán Álvarez <iyan.22.alvarez@gmail.com>
2020-04-17 13:01:36 +0200

    make done after #3 merge

---
+++ build/FondoTrafico.h
@@ -0,0 +1,25 @@
+
+//{{BLOCK(FondoTrafico)
+
+//======================================================================
+//
+//	FondoTrafico, 255x191@16, 
+//	Alphabit on.
+//	+ bitmap not compressed
+//	Total size: 97792 = 97792
+//
+//	Time-stamp: 2020-04-17, 13:01:06
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_FONDOTRAFICO_H
+#define GRIT_FONDOTRAFICO_H
+
+#define FondoTraficoBitmapLen 97792
+extern const unsigned int FondoTraficoBitmap[24448];
+
+#endif // GRIT_FONDOTRAFICO_H
+
+//}}BLOCK(FondoTrafico)
---
+++ build/main.d
@@ -0,0 +1,220 @@
+main.o: /home/parallels/Desktop/StayAtHome-NDS/source/main.c \
+ /opt/devkitpro/libnds/include/nds.h \
+ /opt/devkitpro/libnds/include/nds/libversion.h \
+ /opt/devkitpro/libnds/include/nds/ndstypes.h \
+ /opt/devkitpro/libnds/include/nds/bios.h \
+ /opt/devkitpro/libnds/include/nds/ndstypes.h \
+ /opt/devkitpro/libnds/include/nds/card.h \
+ /opt/devkitpro/libnds/include/nds/ndstypes.h \
+ /opt/devkitpro/libnds/include/nds/debug.h \
+ /opt/devkitpro/libnds/include/nds/dma.h \
+ /opt/devkitpro/libnds/include/nds/interrupts.h \
+ /opt/devkitpro/libnds/include/nds/ipc.h \
+ /opt/devkitpro/libnds/include/nds/memory.h \
+ /opt/devkitpro/libnds/include/nds/system.h \
+ /opt/devkitpro/libnds/include/nds/timers.h \
+ /opt/devkitpro/libnds/include/nds/fifocommon.h \
+ /opt/devkitpro/libnds/include/nds/interrupts.h \
+ /opt/devkitpro/libnds/include/nds/touch.h \
+ /opt/devkitpro/libnds/include/nds/input.h \
+ /opt/devkitpro/libnds/include/nds/sha1.h \
+ /opt/devkitpro/libnds/include/nds/arm9/dynamicArray.h \
+ /opt/devkitpro/libnds/include/nds/arm9/linkedlist.h \
+ /opt/devkitpro/libnds/include/nds/arm9/background.h \
+ /opt/devkitpro/libnds/include/nds/arm9/video.h \
+ /opt/devkitpro/libnds/include/nds/arm9/sassert.h \
+ /opt/devkitpro/libnds/include/nds/memory.h \
+ /opt/devkitpro/libnds/include/nds/dma.h \
+ /opt/devkitpro/libnds/include/nds/arm9/boxtest.h \
+ /opt/devkitpro/libnds/include/nds/arm9/videoGL.h \
+ /opt/devkitpro/libnds/include/nds/arm9/cache.h \
+ /opt/devkitpro/libnds/include/nds/arm9/trig_lut.h \
+ /opt/devkitpro/libnds/include/nds/arm9/math.h \
+ /opt/devkitpro/libnds/include/nds/arm9/dynamicArray.h \
+ /opt/devkitpro/libnds/include/nds/arm9/cache.h \
+ /opt/devkitpro/libnds/include/nds/arm9/console.h \
+ /opt/devkitpro/libnds/include/nds/arm9/background.h \
+ /opt/devkitpro/libnds/include/nds/arm9/decompress.h \
+ /opt/devkitpro/libnds/include/nds/bios.h \
+ /opt/devkitpro/libnds/include/nds/arm9/exceptions.h \
+ /opt/devkitpro/libnds/include/nds/arm9/guitarGrip.h \
+ /opt/devkitpro/libnds/include/nds/arm9/image.h \
+ /opt/devkitpro/libnds/include/nds/arm9/pcx.h \
+ /opt/devkitpro/libnds/include/nds/arm9/input.h \
+ /opt/devkitpro/libnds/include/nds/touch.h \
+ /opt/devkitpro/libnds/include/nds/input.h \
+ /opt/devkitpro/libnds/include/nds/arm9/keyboard.h \
+ /opt/devkitpro/libnds/include/nds/arm9/math.h \
+ /opt/devkitpro/libnds/include/nds/arm9/paddle.h \
+ /opt/devkitpro/libnds/include/nds/arm9/pcx.h \
+ /opt/devkitpro/libnds/include/nds/arm9/piano.h \
+ /opt/devkitpro/libnds/include/nds/arm9/rumble.h \
+ /opt/devkitpro/libnds/include/nds/arm9/sassert.h \
+ /opt/devkitpro/libnds/include/nds/arm9/sound.h \
+ /opt/devkitpro/libnds/include/nds/arm9/sprite.h \
+ /opt/devkitpro/libnds/include/nds/system.h \
+ /opt/devkitpro/libnds/include/nds/arm9/window.h \
+ /opt/devkitpro/libnds/include/nds/arm9/sprite.h \
+ /opt/devkitpro/libnds/include/nds/arm9/trig_lut.h \
+ /opt/devkitpro/libnds/include/nds/arm9/video.h \
+ /opt/devkitpro/libnds/include/nds/arm9/videoGL.h \
+ /opt/devkitpro/libnds/include/nds/arm9/nand.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/fondos.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/sprites.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/defines.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/rutserv.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/teclado.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/temporizadores.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/funciones.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/defines.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/fondos.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/sprites.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/rutserv.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/teclado.h \
+ /home/parallels/Desktop/StayAtHome-NDS/include/temporizadores.h
+
+/opt/devkitpro/libnds/include/nds.h:
+
+/opt/devkitpro/libnds/include/nds/libversion.h:
+
+/opt/devkitpro/libnds/include/nds/ndstypes.h:
+
+/opt/devkitpro/libnds/include/nds/bios.h:
+
+/opt/devkitpro/libnds/include/nds/ndstypes.h:
+
+/opt/devkitpro/libnds/include/nds/card.h:
+
+/opt/devkitpro/libnds/include/nds/ndstypes.h:
+
+/opt/devkitpro/libnds/include/nds/debug.h:
+
+/opt/devkitpro/libnds/include/nds/dma.h:
+
+/opt/devkitpro/libnds/include/nds/interrupts.h:
+
+/opt/devkitpro/libnds/include/nds/ipc.h:
+
+/opt/devkitpro/libnds/include/nds/memory.h:
+
+/opt/devkitpro/libnds/include/nds/system.h:
+
+/opt/devkitpro/libnds/include/nds/timers.h:
+
+/opt/devkitpro/libnds/include/nds/fifocommon.h:
+
+/opt/devkitpro/libnds/include/nds/interrupts.h:
+
+/opt/devkitpro/libnds/include/nds/touch.h:
+
+/opt/devkitpro/libnds/include/nds/input.h:
+
+/opt/devkitpro/libnds/include/nds/sha1.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/dynamicArray.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/linkedlist.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/background.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/video.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/sassert.h:
+
+/opt/devkitpro/libnds/include/nds/memory.h:
+
+/opt/devkitpro/libnds/include/nds/dma.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/boxtest.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/videoGL.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/cache.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/trig_lut.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/math.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/dynamicArray.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/cache.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/console.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/background.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/decompress.h:
+
+/opt/devkitpro/libnds/include/nds/bios.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/exceptions.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/guitarGrip.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/image.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/pcx.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/input.h:
+
+/opt/devkitpro/libnds/include/nds/touch.h:
+
+/opt/devkitpro/libnds/include/nds/input.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/keyboard.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/math.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/paddle.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/pcx.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/piano.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/rumble.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/sassert.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/sound.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/sprite.h:
+
+/opt/devkitpro/libnds/include/nds/system.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/window.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/sprite.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/trig_lut.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/video.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/videoGL.h:
+
+/opt/devkitpro/libnds/include/nds/arm9/nand.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/fondos.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/sprites.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/defines.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/rutserv.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/teclado.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/temporizadores.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/funciones.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/defines.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/fondos.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/sprites.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/rutserv.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/teclado.h:
+
+/home/parallels/Desktop/StayAtHome-NDS/include/temporizadores.h:
